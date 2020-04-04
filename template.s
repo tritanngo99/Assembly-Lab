e7f791655b1f81f91be91389677c4c5be9cb0efd
tritanngo99 <49904863+tritanngo99@users.noreply.github.com>
2020-04-04 08:40:13 +0700

    Create template.s

---
+++ template.s
@@ -0,0 +1,36 @@
+;
+; CS1021 2018/2019	Lab 1
+; 
+
+	AREA	RESET, CODE, READONLY
+	ENTRY
+
+; Test Flag condition
+; Template
+;-------------------------------
+;     Branch equal 
+;equal  --- BEQ
+;not equal --- BNE
+;     unsigned braches
+;less than ---  BLO(or BCC)
+;less than or equal --- BLS
+;greater than or equal --- BHS (or BCS)
+;greater than ---BHI
+;	 signed branches
+;less than --- BLT
+;less than or equal --- BLE
+;greater than or equal --- BGE
+;greater than --- BGT
+;      branch on flags
+;Negative set --- BMI --- Minus
+;Negative clear --- BPL --- Plus
+;Carry set --- BCS (or BHS) --- Carry set
+;Carry clear --- BCC (or BLO) --- Carry Clear
+;Overflow set --- BVS --- Overflow Set
+;Overflow Clear --- BVC --- Overflow Clear
+;Zero Set --- BEQ --- Equal
+;Zero Clear --- BNE --- Not equal
+;-------------------------------
+
+   
+  
